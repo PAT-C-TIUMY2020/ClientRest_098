509f0f11e86554aa6d7c4f34c5119ab5a3f3085a
Nashrul Saefullah <nashrulsaefullah@gmail.com>
2020-12-20 21:29:56 +0700

    Add project files.

---
+++ ClientRest_098_Nashrul/ClassData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Net;
+using static ClientRest_098_Nashrul.Program;
+
+namespace ClientRest_098_Nashrul
+{
+    class ClassData
+    {
+        public void getData()
+        {
+            //var type data yang tidak peduli pada bentuk, tapi isinya
+            var json = new WebClient().DownloadString("http://localhost:1907/Mahasiswa");
+            var data = JsonConvert.DeserializeObject<List<Mahasiswa>>(json);
+
+            foreach (var mhs in data)
+            {
+                Console.WriteLine("NIM :  " + mhs.nama);
+                Console.WriteLine("Nama :  " + mhs.nim);
+                Console.WriteLine("Prodi :  " + mhs.prodi);
+                Console.WriteLine("Angkatan :  " + mhs.angkatan);
+            }
+            Console.ReadLine();
+        }
+    }
+
+    
+}
